cdce23101dd4ec833b4d0089c0d3ff7362421c5b
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-03-04 08:47:21 +0100

    Initialized a spring boot project with the right packages

---
+++ BackEnd/src/main/java/quintor/bioinf/catalog/backend/catalogbackend/BackEndApplication.java
@@ -0,0 +1,14 @@
+package quintor.bioinf.catalog.backend.catalogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/quintor/bioinf/catalog/backend/catalogbackend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package quintor.bioinf.catalog.backend.catalogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
