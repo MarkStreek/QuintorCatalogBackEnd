868986ddf7d431f379e6035c82ba86086c37864a
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-04-04 12:50:45 +0200

    Removed the function to add a spec, not necessary anymore

--- src/main/java/quintor/bioinf/catalog/controller/SpecController.java
+++ src/main/java/quintor/bioinf/catalog/controller/SpecController.java
@@ -1,8 +1,6 @@
 package quintor.bioinf.catalog.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import quintor.bioinf.catalog.entities.Specs;
 import quintor.bioinf.catalog.services.SpecsService;
@@ -22,11 +20,5 @@ public class SpecController {
     @GetMapping
     public List<Specs> getAllSpecs() {
         return specsService.getAllSpecs();
-    }
-
-    @PostMapping("/add")
-    public ResponseEntity<String> addSpecs(@RequestBody @Valid Specs specs) {
-        specsService.addSpecs(specs);
-        return ResponseEntity.ok("Added spec");
     }
 }
