a085d9082c26dc0af0aee0a44227132037deca09
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-04-09 15:24:50 +0200

    Created some new Exceptionshandlers for the @RestControllerAdvice class

--- src/main/java/quintor/bioinf/catalog/controller/ExceptionController.java
+++ src/main/java/quintor/bioinf/catalog/controller/ExceptionController.java
@@ -1,19 +1,53 @@
 package quintor.bioinf.catalog.controller;
 
+import jakarta.validation.ConstraintDeclarationException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.io.IOException;
 import java.util.Date;
 
 @RestControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(value = {Exception.class})
+    @ExceptionHandler(value = {NullPointerException.class})
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage nullPointerException(Exception ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(true));
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ErrorMessage exception(Exception ex, WebRequest request) {
+    public ErrorMessage illegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(true));
+    }
+
+    @ExceptionHandler(value = {IllegalStateException.class})
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorMessage illegalStateException(IllegalStateException ex, WebRequest request) {
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(true));
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorMessage legalStateException(IllegalStateException ex, WebRequest request) {
         return new ErrorMessage(
                 HttpStatus.BAD_REQUEST.value(),
                 new Date(),
