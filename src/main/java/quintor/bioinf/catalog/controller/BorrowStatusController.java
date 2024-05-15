6affc80e4b77bab2634dfe3ad6e20bd82f8435fc
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-05-15 17:04:11 +0200

    Created a controller especially for BorrowStatus. This controller has endpoints for adding a new request and retrieving single and all records from the database

--- src/main/java/quintor/bioinf/catalog/controller/BorrowStatusController.java
+++ src/main/java/quintor/bioinf/catalog/controller/BorrowStatusController.java
@@ -1,13 +1,13 @@
 package quintor.bioinf.catalog.controller;
 
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import quintor.bioinf.catalog.dto.BorrowDTO;
-import quintor.bioinf.catalog.entities.BorrowedStatus;
+import quintor.bioinf.catalog.dto.BorrowRequest;
 import quintor.bioinf.catalog.services.BorrowedStatusService;
 
 import java.util.Date;
@@ -26,15 +26,39 @@ public class BorrowStatusController {
     }
 
     @PostMapping
-    public ReturnMessage borrowDevice(@RequestBody BorrowDTO borrowDTO) {
+    public ReturnMessage borrowDevice(@RequestBody BorrowRequest borrowRequest, HttpServletRequest request) {
+        // Log the incoming request
+        log.info("New incoming request. CLIENT IP: {}, PORT: {}, REQUEST URI: {}",
+                request.getRemoteAddr(), request.getRemotePort(), request.getRequestURI());
 
+        // Create a new borrow request with the incoming "BorrowRequest" object
+        // BorrowRequest is a simple (DTO) object that contains the username and device id
+        this.borrowedStatusService.borrowDevice(
+                borrowRequest.getUserName(),
+                borrowRequest.getDeviceId());
 
-        borrowedStatusService.borrowDevice(borrowDTO);
-
+        // Return a new (OK) message to the client
         return new ReturnMessage(
                 HttpStatus.OK.value(),
                 new Date(),
-                "Device borrowed",
-                "Device borrowed");
+                "Apparaat succesvol uitgeleend",
+                "Een nieuw apparaat is succesvol toegevoegd aan de database en " +
+                        "alle checks zijn uitgevoerd");
+    }
+
+    @GetMapping("/{id}")
+    public BorrowDTO getBorrowStatus(@PathVariable Long id) {
+        return borrowedStatusService.getBorrowStatus(id);
+    }
+
+    /**
+     * Get endpoint to get all borrowed devices. All the borrowed devices are converted to a BorrowDTO object.
+     * The BorrowDTO's are placed in a list and returned to the client.
+     *
+     * @return List<BorrowDTO> list of borrowed devices
+     */
+    @GetMapping()
+    public List<BorrowDTO> getAllBorrowStatus() {
+        return borrowedStatusService.getAllBorrowStatus();
     }
 }
