9c294513a3463f712fd3f915a413e03d6313c148
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-05-17 12:16:00 +0200

    Added more validation error messages to the DeviceDTO.java

--- src/main/java/quintor/bioinf/catalog/dto/DeviceDTO.java
+++ src/main/java/quintor/bioinf/catalog/dto/DeviceDTO.java
@@ -16,22 +16,22 @@ public class DeviceDTO {
 
     @NotNull
     @NotEmpty
-    @Length(min = 1, max = 50)
+    @Length(min = 1, max = 50, message = "Type moet tussen de 1 en 50 karakters lang zijn")
     private String type;
 
     @NotNull
     @NotEmpty
-    @Length(min = 1, max = 50)
+    @Length(min = 1, max = 50, message = "Merk moet tussen de 1 en 50 karakters lang zijn")
     private String brandName;
 
     @NotNull
     @NotEmpty
-    @Length(min = 1, max = 50)
+    @Length(min = 1, max = 50, message = "Model moet tussen de 1 en 50 karakters lang zijn")
     private String model;
 
     @NotNull
-    @NotEmpty
-    @Length(min = 3, max = 50)
+    @NotEmpty(message = "Serienummer mag niet leeg zijn")
+    @Length(min = 3, max = 50, message = "Serienummer moet tussen de 3 en 50 karakters lang zijn")
     private String serialNumber;
 
     @NotNull
@@ -52,8 +52,8 @@ public class DeviceDTO {
     @NotEmpty
     private String locationName;
 
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "Specificaties mogen niet leeg zijn")
+    @NotEmpty(message = "Specificaties mogen niet leeg zijn")
     private List<SpecDetail> specs;
 
 }
